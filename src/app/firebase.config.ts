a0c7f270c3d30bc9cfab87c3e445e5bfafc31e20
abdelmoughitdrissi <40865579+abdelmoughitdrissi@users.noreply.github.com>
2019-04-01 21:10:59 +0100

    creating web
    test

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,20 @@
+export const firebaseConfig= {
+    // apiKey: "AIzaSyDr52kdm1g6xmuSIedc3dHgO1zBALD7olo",
+    // authDomain: "resaturant-app.firebaseapp.com",
+    // databaseURL: "https://resaturant-app.firebaseio.com",
+    // projectId: "resaturant-app",
+    // storageBucket: "resaturant-app.appspot.com",
+    // messagingSenderId: "776845960893"
+    apiKey: "AIzaSyAmHvzwv4999iu2Z-ucvzWEQtWVbpniQIU",
+    authDomain: "skyjet-59885.firebaseapp.com",
+    databaseURL: "https://skyjet-59885.firebaseio.com",
+    projectId: "skyjet-59885",
+    storageBucket: "skyjet-59885.appspot.com",
+    messagingSenderId: "613467106093"
+  };
+  
+export const stripeKey = 'pk_test_6pRNASCoBOKtIshFeQd4XMUh';
+
+export const shareLink = 'https://restaurantwebapps.firebaseapp.com';//'https://moneysave-fabea.firebaseapp.com';//
+
+export const instagramURL = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=6646817240.509e952.4b358013bca4483b8a162808ce020e07';
